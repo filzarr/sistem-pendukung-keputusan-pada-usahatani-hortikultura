d508d0d1ae71fd605ce2bc46a993d27e7c6718b3
filzarr <filzaramadhan2003@students.usu.ac.id>
2024-06-27 05:32:52 +0700

    change: change design homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,51 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import { Carousel } from "flowbite-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-  <main className='sm:px-[32px] md:px-[64px] py-[64px] grid grid-cols-1 lg:grid-cols-2 gap-8 items-center'>
-    <div className=" space-y-8">
-      <p className='font-bold text-[#65B741] text-[32px]'>Sistem Pendukung Keputusan Pada Usaha Tani Hidorkultura</p>
-      <p className='text-[#7E7E7E] font-medium capitalize text-[16px]'>hadir untuk membantu para petani dalam mengambil keputusan terbaik dalam usaha tani hidrokultura. Dengan menggunakan teknologi canggih dan data analitik, kami menyediakan solusi yang efisien dan efektif untuk meningkatkan produktivitas serta kualitas hasil tani Anda.</p>
-       
-    </div>
-    <div className=" overflow-hidden grid grid-cols-2 gap-4   h-[359px] rounded">
-      <div className=" overflow-hidden ">
-        <Image alt='Gambar Pohon Tomat' src={'/bg-1.jpg'} width={500} height={500} className=' object-contain object-center' />
+    <>
+      <div className=" relative     h-[595px]">
+        <Carousel
+          className="contrast-50 brightness-50 absolute top-0 bottom-0 min-h-full min-w-full"
+          indicators={false}
+        >
+          <img
+            src="/1.jpg"
+            alt="..."
+          />
+          <img
+            src="/2.jpg"
+            alt="..."
+          /> 
+          <img
+            src="/3.jpg"
+            alt="..."
+          /> 
+          <img
+            src="/4.jpg"
+            alt="..."
+          /> 
+          <img
+            src="/5.jpg"
+            alt="..."
+          />  
+        </Carousel>
+        <div className=" absolute top-0 left-0 right-0 min-h-full min-w-full flex items-center flex-col justify-center sm:px-[32px] md:px-[128px] py-[64px]  space-y-6">
+          <p className="font-bold text-[#80f54e] text-[32px] text-center">
+            Sistem Pendukung Keputusan Pada Usaha Tani Hidorkultura
+          </p>
+          <p className="text-[#fff] font-medium capitalize text-[16px] text-center">
+            hadir untuk membantu para petani dalam mengambil keputusan terbaik
+            dalam usaha tani hidrokultura. Dengan menggunakan teknologi canggih
+            dan data analitik, kami menyediakan solusi yang efisien dan efektif
+            untuk meningkatkan produktivitas serta kualitas hasil tani Anda.
+          </p>
+          <div className="flex justify-center">
+          <Link href={'/about'} className='p-[12px] px-[24px] text-white font-bold bg-[#65B741] rounded'>Contact US</Link>
+          </div>
+        </div>
       </div>
-      <div className=" ">
-        <Image alt='Gambar Pohon Tomat' src={'/gambar-cabe.jpg'}width={500} height={500} className=' object-contain object-center' />
-      </div>
-    </div>
-  </main>
-  )
+    </>
+  );
 }
